9c1fef92d36e3983ddcfbb80a11fa6560f27fc37
BL <bhushankumar.lilapara@gmail.com>
2018-09-28 21:13:35 +0530

    Add search by Category API call

---
+++ examples/javaScript/commerce/catalog/searchByCategory.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var clientId = process.env.EBAY_CLIENT_ID || 'YOUR_KEY';
+var clientSecret = process.env.EBAY_CLIENT_SECRET || 'YOUR_SECRET';
+
+var eBay = require('../../../../lib/eBay-node-client')(clientId, clientSecret);
+
+var utils = require('../../../javaScript/utils');
+
+var catalogRequest = async function () {
+    var userToken = utils.USER_TOKEN;
+    eBay.setUserToken(userToken);
+
+    var data = {
+        // Leaf Category Name
+        q: 'Video Cables',
+        // Leaf Category Id
+        category_ids: '67859'
+    };
+    try {
+        var response = await eBay.catalog.search(data);
+        console.log('response', response);
+    } catch (error) {
+        console.log('error ', error);
+        return;
+    }
+};
+
+catalogRequest();
